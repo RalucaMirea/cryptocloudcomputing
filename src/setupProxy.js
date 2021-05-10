64d4a1771e5b2e6d2b0e2064ee088d806cadb63e
RalucaMirea <raluca.miream@gmail.com>
2021-05-10 21:08:59 +0300

    proxycommit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('https://finnhub.io/api', 
-        { target: 'https://github.com/RalucaMirea/cryptocloudcomputing.git/' } // change with https://github.com/RalucaMirea/cryptocloudcomputing.git if it's not working
+        { target: 'https://stockmarketcloudcomputing.s3-website.eu-central-1.amazonaws.com/' } // change with https://github.com/RalucaMirea/cryptocloudcomputing.git if it's not working
     ));
     app.use(proxy('https://api.nomics.com/v1', 
         { target: 'https://stockmarketcloudcomputing.s3-website.eu-central-1.amazonaws.com//' }  // change with http://stockmarketcloudcomputing.s3-website.eu-central-1.amazonaws.com if it's not working
